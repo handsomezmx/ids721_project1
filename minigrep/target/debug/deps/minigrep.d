6be5bbe5f9eba258bba0ae3147774bf2b735bd2a
Mingxuan Zhao <mzhao@ucsd.edu>
2023-01-25 00:47:58 -0500

    argument passin

---
+++ minigrep/target/debug/deps/minigrep.d
@@ -0,0 +1,5 @@
+C:\Users\zmxsz\Desktop\ids721\ids721_project1\minigrep\target\debug\deps\minigrep.exe: src\main.rs
+
+C:\Users\zmxsz\Desktop\ids721\ids721_project1\minigrep\target\debug\deps\minigrep.d: src\main.rs
+
+src\main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+C:\Users\zmxsz\Desktop\ids721\ids721_project1\minigrep\target\debug\minigrep.exe: C:\Users\zmxsz\Desktop\ids721\ids721_project1\minigrep\src\main.rs
